9c80409cd6cf9ba5debcbfc1fc2c11f1a25f860e
welcome1208 <hyyi1@naver.com>
2024-05-03 16:25:30 +0900

    정리

--- NewFront/react-tapamadre-app/src/components/ProfileNavBar.js
+++ NewFront/react-tapamadre-app/src/components/ProfileNavBar.js
@@ -4,32 +4,16 @@ import { Link } from 'react-router-dom'
 const ProfileNavBar = () => {
     return (
         <div className="col-md-4 col-lg-3 order-md-1">
-            {/* 공통 설정 탭 */}
+            {/* 관리자기능 탭 */}
+            {/* 관리자 체크후 표시하기 */}
             <div className="position-relative mb-5">
-                <h5 className="mb-3">프로필</h5>
+                <h5 className="mb-3">관리자 기능</h5>
                 <ul className="list-unstyled">
                     <li>
                         <Link to="/mypage/myaccount" className="text-secondary d-block py-1">
                             계정관리
                         </Link>
                     </li>
-                    <li>
-                        <Link to="/mypage/myreservation" className="text-secondary d-block py-1">
-                            예약관리
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/mypage/myinquiry" className="text-secondary d-block py-1">
-                            문의사항
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            {/* 관리자기능 탭 */}
-            {/* 관리자 체크후 표시하기 */}
-            <div className="position-relative mb-5">
-                <h5 className="mb-3">관리자 기능</h5>
-                <ul className="list-unstyled">
                     <li>
                         <Link to="/admin/newsevent" className="text-secondary d-block py-1">
                             블로그관리
@@ -38,21 +22,6 @@ const ProfileNavBar = () => {
                     <li>
                         <Link to="/admin/menu" className="text-secondary d-block py-1">
                             메뉴관리
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/reservation" className="text-secondary d-block py-1">
-                            예약관리
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/inquiries" className="text-secondary d-block py-1">
-                            문의사항
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="member" className="text-secondary d-block py-1">
-                            회원관리
                         </Link>
                     </li>
                 </ul>
--- NewFront/react-tapamadre-app/src/routes/index.js
+++ NewFront/react-tapamadre-app/src/routes/index.js
@@ -11,32 +11,20 @@ import Setting from '../pages/profile/Setting'
 // 프로필 세부 중첩 라우팅
 // 관리자
 // 예약
-import AdminReservationUpdate from '../pages/profile/admin/reservation/AdminReservationUpdate'
-import AdminReservationList from '../pages/profile/admin/reservation/AdminReservationList'
+
 // News/Event
 import AdminNewseventList from '../pages/profile/admin/newsevent/AdminNewseventList'
 import AdminNewseventUpdate from '../pages/profile/admin/newsevent/AdminNewseventUpdate'
 import AdminNewseventCreate from '../pages/profile/admin/newsevent/AdminNewseventCreate'
 // 문의사항
-import AdminInquiriesList from '../pages/profile/admin/inquiries/AdminInquiriesList'
-import AdminInquiriesAnswer from '../pages/profile/admin/inquiries/AdminInquiriesAnswer'
 // 회원
-import AdminMemberList from '../pages/profile/admin/member/AdminMemberList'
-import AdminMemberCreate from '../pages/profile/admin/member/AdminMemberCreate'
-import AdminMemberUpdate from '../pages/profile/admin/member/AdminMemberUpdate'
 // 메뉴
 import AdminMenuList from '../pages/profile/admin/menu/AdminMenuList'
 import AdminMenuCreate from '../pages/profile/admin/menu/AdminMenuCreate'
 import AdminMenuUpdate from '../pages/profile/admin/menu/AdminMenuUpdate'
-
 // 나의정보
 // 계정관리
 import MyPersonalData from '../pages/profile/mypage/MyPersonalData'
-// 예약관리
-import MyReservations from '../pages/profile/mypage/MyReservations'
-// 문의사항
-import MyInquiries from '../pages/profile/mypage/MyInquiries'
-import MyInquiryDetail from '../pages/profile/mypage/MyInquiryDetail'
 
 //로그인 인증 여부를 체크하는 함수
 const AuthProtected = (props) => {
@@ -65,19 +53,12 @@ const SwitchRoutes = () => {
                     {/* 사용자 페이지 컴포넌트 라우팅 */}
                     <Route path="/mypage" element={<Setting />}>
                         {/* 계정관리 */}
+
                         <Route path="myaccount" element={<MyPersonalData />} />
-                        {/* 예약관리 */}
-                        <Route path="myreservation" element={<MyReservations />} />
-                        {/* 문의사항 */}
-                        <Route path="myinquiry" element={<MyInquiries />} />
-                        <Route path="myinquiry/:idx" element={<MyInquiryDetail />} />
                     </Route>
 
                     {/* 관리자 페이지 컴포넌트 라우팅 */}
                     <Route path="/admin/" element={<Setting />}>
-                        {/* 예약 */}
-                        <Route path="reservation" element={<AdminReservationList />} />
-                        <Route path="reservation/update" element={<AdminReservationUpdate />} />
                         {/* news/event */}
                         <Route path="newsevent" element={<AdminNewseventList />} />
                         <Route path="newsevent/update/:article_id" element={<AdminNewseventUpdate />} />
@@ -86,13 +67,6 @@ const SwitchRoutes = () => {
                         <Route path="menu" element={<AdminMenuList />} />
                         <Route path="menu/update/" element={<AdminMenuUpdate />} />
                         <Route path="menu/create" element={<AdminMenuCreate />} />
-                        {/* 문의사항 */}
-                        <Route path="inquiries" element={<AdminInquiriesList />} />
-                        <Route path="inquiries/answer" element={<AdminInquiriesAnswer />} />
-                        {/* 회원 */}
-                        <Route path="member" element={<AdminMemberList />} />
-                        <Route path="member/update" element={<AdminMemberUpdate />} />
-                        <Route path="member/create" element={<AdminMemberCreate />} />
                     </Route>
                 </Routes>
             </Suspense>
